754f4741e66386aee1f2698e7491e99007d26f84
Karl Santiago Bernaldez <tbsi.karl@gmail.com>
2024-11-29 11:55:36 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="text-wrapper">BAKAL AKO!!!</div>
+
+        <p className="p">
+          Always be cautious of unsolicited emails, avoid clicking suspicious
+          links, and verify requests to protect yourself from phishing attacks.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
